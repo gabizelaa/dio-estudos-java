5efbdda7f35c1faee37f3da1abe550f8e8b22c7e
gabizelaa <madonamia68@gmail.com>
2024-04-26 14:37:12 -0300

    .

--- src/ExemploBreakContinue.java
+++ src/ExemploBreakContinue.java
@@ -3,6 +3,7 @@ public class ExemploBreakContinue {
         for (int numero = 1; numero <= 5; numero++){
             if(numero == 3)
             break;
+            //ou continue
 
             System.out.println(numero);
         }
